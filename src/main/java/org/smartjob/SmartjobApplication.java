ba8b1ff27f6e3992da3a5955fd93c2006c061c33
JeremyDeAvila <jeremy.deavila@insoftar.com>
2025-11-08 17:50:45 -0500

    commit inicial

---
+++ src/main/java/org/smartjob/SmartjobApplication.java
@@ -0,0 +1,13 @@
+package org.smartjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartjobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartjobApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/smartjob/SmartjobApplicationTests.java
@@ -0,0 +1,13 @@
+package org.smartjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartjobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
